ef3e7c47c092e8c9b2f7aeffd0fae565856ab26f
Vikrant <tiwarivikrant9764@gmail.com>
2025-01-23 17:57:17 +0530

    removed old setups

--- src/components/about_section/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function AboutSection() {
-    return (
-        <section>
-            About Section
-        </section>
-    )
-}
--- src/components/skill_section/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function SkillSection() {
-    return (
-        <section>
-            Skill Section
-        </section>
-    )
-}
