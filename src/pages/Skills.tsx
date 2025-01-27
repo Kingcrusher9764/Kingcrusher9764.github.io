13bc4bf0e19b975b16c94dad0ca67432a6ef8469
Vikrant <tiwarivikrant9764@gmail.com>
2025-01-27 17:40:09 +0530

    added links to the skills

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -8,22 +8,22 @@ export default function Skills() {
             <div className="w-11/12">
                 <TitleBox title={"Skills"} />
 
-                <div className="flex flex-row flex-wrap justify-start gap-[10px] border-2 border-green-500 p-4 rounded-xl">
+                <div className="flex flex-row flex-wrap justify-center gap-[10px] border-2 border-green-500 p-4 rounded-xl">
 
-                    <SkillBox skillName="Node.js" skillImage="/icons/nodejs.png" />
-                    <SkillBox skillName="React" skillImage="/icons/react.png" />
-                    <SkillBox skillName="Next.Js" skillImage="/icons/nextjs.png" />
-                    <SkillBox skillName="Django" skillImage="/icons/django.png" />
-                    <SkillBox skillName="MongoDB" skillImage="/icons/mongodb.png" />
-                    <SkillBox skillName="PostgreSQL" skillImage="/icons/postgresql.png" />
-                    <SkillBox skillName="Docker" skillImage="/icons/docker.png" />
-                    <SkillBox skillName="Redux" skillImage="/icons/redux.png" />
-                    <SkillBox skillName="Git" skillImage="/icons/git.png" />
-                    <SkillBox skillName="GitHub" skillImage="/icons/github.png" />
-                    <SkillBox skillName="HTML" skillImage="/icons/html.png" />
-                    <SkillBox skillName="CSS" skillImage="/icons/css.png" />
-                    <SkillBox skillName="Javascript" skillImage="/icons/javascript.png" />
-                    <SkillBox skillName="Python" skillImage="/icons/python.png" />
+                    <SkillBox skillName="Node.js" skillImage="/icons/nodejs.png" url="https://nodejs.org/en" />
+                    <SkillBox skillName="React" skillImage="/icons/react.png" url="https://react.dev/" />
+                    <SkillBox skillName="Next.Js" skillImage="/icons/nextjs.png" url="https://nextjs.org/" />
+                    <SkillBox skillName="Django" skillImage="/icons/django.png" url="https://www.djangoproject.com/" />
+                    <SkillBox skillName="MongoDB" skillImage="/icons/mongodb.png" url="https://www.mongodb.com/" />
+                    <SkillBox skillName="PostgreSQL" skillImage="/icons/postgresql.png" url="https://www.postgresql.org/" />
+                    <SkillBox skillName="Docker" skillImage="/icons/docker.png" url="https://www.docker.com/" />
+                    <SkillBox skillName="Redux" skillImage="/icons/redux.png" url="https://redux-toolkit.js.org" />
+                    <SkillBox skillName="Git" skillImage="/icons/git.png" url="https://git-scm.com/" />
+                    <SkillBox skillName="GitHub" skillImage="/icons/github.png" url="https://www.github.com" />
+                    <SkillBox skillName="HTML" skillImage="/icons/html.png" url="https://developer.mozilla.org/en-US/docs/Web/HTML" />
+                    <SkillBox skillName="CSS" skillImage="/icons/css.png" url="https://developer.mozilla.org/en-US/docs/Web/CSS" />
+                    <SkillBox skillName="Javascript" skillImage="/icons/javascript.png" url="https://developer.mozilla.org/en-US/docs/Web/JavaScript" />
+                    <SkillBox skillName="Python" skillImage="/icons/python.png" url="https://www.python.org/" />
 
                 </div>
             </div>
@@ -31,9 +31,19 @@ export default function Skills() {
     )
 }
 
-export function SkillBox({ skillName, skillImage }: { skillName: string; skillImage: string; }) {
+export function SkillBox({
+    skillName, skillImage, url,
+}: {
+    skillName: string;
+    skillImage: string;
+    url?: string;
+}) {
     return (
-        <div className="w-[max-content]">
+        <a
+            target="_blank"
+            href={url}
+            className="w-[max-content]"
+        >
             <div className="bg-blue-400 rounded-xl p-2 w-[120px] h-[120px] flex items-center justify-center">
                 <img
                     src={skillImage}
@@ -43,6 +53,6 @@ export function SkillBox({ skillName, skillImage }: { skillName: string; skillIm
             </div>
 
             <div className="text-center mt-4 text-3xl">{skillName}</div>
-        </div>
+        </a>
     )
 }
